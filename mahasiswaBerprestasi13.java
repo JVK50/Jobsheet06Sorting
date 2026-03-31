edde9343912ad00a9c99d602f0b9e2ab57b841d1
unknown <jidanali32@gmail.com>
2026-03-31 11:07:20 +0700

    5.3.5 urutan terkecil

--- mahasiswaBerprestasi13.java
+++ mahasiswaBerprestasi13.java
@@ -41,5 +41,21 @@ public class mahasiswaBerprestasi13 {
             }
         }
     }
+
+    void selectionSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            int idxMin = i;
+            for (int j = i + 1; j < listMhs.length; j++) {
+                if (listMhs[j] != null && listMhs[idxMin] != null) {
+                    if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                        idxMin = j;
+                    }
+                }
+            }
+            mahasiswa13 tmp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = tmp;
+        }
+    }
 }
 
--- mahasiswaMain13.java
+++ mahasiswaMain13.java
@@ -44,3 +44,24 @@ public class mahasiswaMain13 {
         input.close();
     }
 }
+
+class MahasiswaDemo13 {
+    public static void main(String[] args) {
+        mahasiswaBerprestasi13 list = new mahasiswaBerprestasi13(5);
+        mahasiswa13 m1 = new mahasiswa13("123", "Zidan", "2A", 3.2);
+        mahasiswa13 m2 = new mahasiswa13("124", "Ayu", "2A", 3.5);
+        mahasiswa13 m3 = new mahasiswa13("125", "Sofi", "2A", 3.1);
+        mahasiswa13 m4 = new mahasiswa13("126", "Sita", "2A", 3.9);
+        mahasiswa13 m5 = new mahasiswa13("127", "Miki", "2A", 3.7);
+
+        list.tambah(m1);
+        list.tambah(m2);
+        list.tambah(m3);
+        list.tambah(m4);
+        list.tambah(m5);
+
+        System.out.println("Data yang sudah terutur menggunakan SELECTION SORT (ASC)");
+        list.selectionSort();
+        list.tampil();
+    }
+}
