9a32822762753e916c2b24c964c0dfbc47b3465d
unknown <jidanali32@gmail.com>
2026-03-31 09:27:23 +0700

    commit

--- sortingMain13.java
+++ sortingMain13.java
@@ -1,12 +1,15 @@
 package Praktikum5;
 
 public class sortingMain13 {
-    int a[] = {20, 10, 7, 2, 12};
-
-    sorting13 dataurut1 = new sorting13(a, a.length);
-
     public static void main(String[] args) {
-        System.out.println("Data awal 1");
+        int a[] = {20, 10, 7, 2, 12};
+    
+        sorting13 dataurut1 = new sorting13(a, a.length);
         
+        System.out.println("Data awal 1");
+        dataurut1.tampilData();
+        dataurut1.bubbleSort();
+        System.out.println("Data setelah diurutkan dengan bubble sort(ASC)");
+        dataurut1.tampilData();
     }
 }
